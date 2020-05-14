d6cc8b178de96d7afa9756cd24034dce0b80bc71
BeccaMorse <beccamorse@gmail.com>
2020-05-14 11:01:33 -0400

    Create first square

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
     var canvas = new fabric.Canvas('myCanvas');
     // create a rectangle object
     var rect = new fabric.Rect({
-      left: 100,
-      top: 100,
-      fill: 'red',
-      width: 20,
-      height: 20
+      left: 50,
+      top: 50,
+      stroke: 'red',
+      fill: 'white',
+      width: 200,
+      height: 200
     });
     // "add" rectangle onto canvas
     canvas.add(rect);
@@ -17,4 +18,25 @@ document.addEventListener("DOMContentLoaded", () => {
     // ctx.moveTo(0, 0);
     // ctx.lineTo(1600, 800);
     // ctx.stroke();
+
+    var spaceTitle = new fabric.Textbox("Standing Ovation: ", {
+      fontWeight: 'bold',
+      fontSize: 20,
+      width: 200,
+      height: 30,
+      textAlign: 'center',
+      top: 50,
+      left: 50
+    });
+    var spaceInstruction = new fabric.Textbox("All the other family members " + 
+    "will applaud and cheer while you take a bow. ", {
+      fontWeight: 'normal',
+      fontSize: 18,
+      top: 80,
+      left: 55,
+      width: 200,
+      height: 170
+    });
+    canvas.add(spaceTitle);
+    canvas.add(spaceInstruction);
 })
