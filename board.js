32052d6370ce33e46f78a10cdb04646d0f34003d
Russell Emmer <me@russelleh.info>
2020-06-23 09:32:50 -0400

    Test board moves player in a loop

--- board.js
+++ board.js
@@ -4,7 +4,7 @@ var Player = require('./player.js')
 module.exports = class Board {
     constructor(space_data, player_data) {
         this.spaces = new Array
-        for (i = 0; i < space_data.length; i++) {
+        for (var i = 0; i < space_data.length; i++) {
             this.spaces.push(new Space(
               space_data[i].title,
               space_data[i].instructions,
@@ -13,7 +13,7 @@ module.exports = class Board {
             )
         }
         this.players = new Array
-        for (i = 0; i < player_data.length; i++) {
+        for (var i = 0; i < player_data.length; i++) {
             this.players.push(new Player(
                 player_data[i].color,
                 player_data[i].x,
